568366832b405d67bd0e2c5dfe102e6eefeae4ce
randomusert <eetupajamaki@gmail.com>
2025-11-07 19:49:55 +0200

    recipes

--- src/main/java/io/github/randomusert/mods/tincraft/TinCraft.java
+++ src/main/java/io/github/randomusert/mods/tincraft/TinCraft.java
@@ -2,27 +2,9 @@ package io.github.randomusert.mods.tincraft;
 
 import io.github.randomusert.mods.tincraft.block.TinBlock;
 import io.github.randomusert.mods.tincraft.config.Config;
-import io.github.randomusert.mods.tincraft.item.ItemTin;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
-import net.minecraft.world.item.crafting.CraftingBookCategory;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.ShapedRecipe;
-import net.minecraft.world.item.crafting.ShapedRecipePattern;
 import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -30,18 +12,9 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
-import io.github.randomusert.mods.tincraft.config.Config;
-
-
 import java.util.List;
 import java.util.Map;
-
 import static io.github.randomusert.mods.tincraft.item.ItemTin.TIN_INGOT;
 import static io.github.randomusert.mods.tincraft.item.ItemTin.TIN_ITEMS;
 
@@ -67,11 +40,6 @@ public class TinCraft {
         LOGGER.info("loading tincraft client side");
     }
 
-    // Add the example block item to the building blocks tab
-
-
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
